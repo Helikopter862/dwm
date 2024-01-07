ee34d9a26705fdb4a12cf75e140100a1a9904e33
Helikopter862 <130805628+Helikopter862@users.noreply.github.com>
2024-01-08 00:37:42 +0100

    Update config.h

--- config.h
+++ config.h
@@ -65,12 +65,13 @@ static const Layout layouts[] = {
 /* commands */
 static const char *launchercmd[] = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *flamecmd[] = { "flameshot", "gui", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = launchercmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("maim -s -u | xclip -selection clipboard -t image/png -i") },
+	{ MODKEY,                       XK_Print,  spawn,          {.v = flamecmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
